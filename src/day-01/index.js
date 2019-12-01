3af5482d2c228a673051b269fd57653de01f1a95
Lur <minecraft2314@gmail.com>
2019-12-01 20:05:17 +1300

    Day 1

--- src/day-01/index.js
+++ src/day-01/index.js
@@ -1,3 +1,26 @@
 const fs = require('fs')
 let input = fs.readFileSync('input.txt', 'utf8')
-const _ = require('lodash')
+
+// Part 1
+let sum = input
+    .split('\n')
+    .map(Number)
+    .map(mass => Math.floor(mass / 3) - 2)
+    .reduce((a, b) => a + b)
+
+console.log(sum)
+
+// Part 2
+const getFuel = mass => {
+    let fuel = Math.max(0, Math.floor(mass / 3) - 2)
+    if (fuel) fuel += getFuel(fuel)
+    return fuel
+}
+
+sum = input
+    .split('\n')
+    .map(Number)
+    .map(getFuel)
+    .reduce((a, b) => a + b)
+
+console.log(sum)
